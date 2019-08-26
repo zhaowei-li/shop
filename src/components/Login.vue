e587ce72e394e51dc6b2cb09a137ba1d09418b9f
weili <zhaoweili1531@gmail.com>
2019-08-26 20:29:46 +0800

    登陆页完成

---
+++ src/components/Login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login">
+    <el-form class="from" ref="form" status-icon :rules="rules" :model="form" label-width="80px">
+      <img class="login-img" src="../assets/avatar.jpg" alt />
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-btn" @click="login">登陆</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'change' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'change' },
+          {
+            min: 6,
+            max: 12,
+            message: '长度在 6 到 12 个字符',
+            trigger: 'change'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm () {
+      console.log(this.$refs.form)
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate(valid => {
+        console.log(this.form)
+        if (!valid) return
+        axios({
+          method: 'post',
+          url: 'http://localhost:8888/api/private/v1/login',
+          data: this.form
+        }).then(res => {
+          console.log(res.data)
+          const { meta } = res.data
+          console.log(meta)
+          if (meta.status === 200) {
+            this.$message({
+              message: '登陆成功',
+              type: 'success',
+              info: 1000
+            })
+            this.$router.push({ name: 'index' })
+          } else {
+            this.$message.error('登陆失败')
+          }
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+
+  .from {
+    width: 400px;
+    margin: 0 auto;
+    margin-top: 200px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 20px;
+    padding-top: 70px;
+    position: relative;
+
+    .login-btn {
+      margin-right: 70px;
+    }
+
+    .login-img {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -70px;
+      border-radius: 50%;
+      border: 5px solid #fff;
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './components/Login.vue'
+import Index from './components/Index.vue'
 
 Vue.use(Router)
 
@@ -8,7 +9,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Home
+      redirect: 'login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: Index
     }
   ]
 })
