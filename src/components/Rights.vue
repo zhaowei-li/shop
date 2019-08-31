fd48180040bb60d841acf75108bc845550c3845f
weili <zhaoweili1531@gmail.com>
2019-08-31 15:22:46 +0800

    权限列表完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="rights">这是Rights内容</div>
+  <div class="rights">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 列表 -->
+    <el-table :data="rightsList" style="width: 100%">
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="authName" label="权限名称"></el-table-column>
+      <el-table-column prop="path" label="路径"></el-table-column>
+      <el-table-column label="层级">
+        <template v-slot:default="{ row }">
+          <span v-if="row.level === '0'">一级</span>
+          <span v-if="row.level === '1'">二级</span>
+          <span v-if="row.level === '2'">三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  created () {
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList () {
+      const { meta, data } = await this.$axios.get(`rights/list`)
+      if (meta.status === 200) {
+        this.rightsList = data
+      } else {
+        this.$message.error(meta.msg)
+      }
+    }
+  }
+}
 </script>
 
 <style>
